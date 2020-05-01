d4ea6332b6575f821f7ba8e4e9e12751afd6d270
Luguaa <893821635@qq.com>
2020-05-01 21:30:54 +0800

    添加了文章详情页，分开了文章列表页和首页

--- article/urls.py
+++ article/urls.py
@@ -4,10 +4,11 @@ __date__ = '2020/4/30 13:20'
 
 from django.contrib import admin
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView
+from .views import IndexView, ArticleDetailView, ArticleListView
 
 app_name = 'article'
 urlpatterns = [
-    path('', ArticleListView.as_view(), name='article_list'),
-    path('article/<int:article_id>', ArticleDetailView.as_view(), name='article_detail' )
+    path('', IndexView.as_view(), name='index'),
+    path('article/<int:article_id>', ArticleDetailView.as_view(), name='article_detail'),
+    path('article_list', ArticleListView.as_view(), name='article_list')
 ]
--- article/views.py
+++ article/views.py
@@ -1,3 +1,4 @@
+import markdown
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 from django.shortcuts import render
@@ -6,7 +7,7 @@ from django.views.generic.base import View
 from .models import Article, Channels, Banner, Tags, Video
 
 
-class ArticleListView(View):
+class IndexView(View):
     """
     文章列表页、首页
     """
@@ -79,3 +80,47 @@ class ArticleDetailView(View):
         return render(request, 'post.html', {
             'article': article,
         })
+
+
+class ArticleListView(View):
+    """
+    文章列表页、首页
+    """
+    def get(self, request):
+        all_articles = Article.objects.all().order_by("-add_time")
+        hot_articles = Article.objects.all().order_by("-vote_num")[:5]
+
+        # # 关键词搜索
+        # search_keywords = request.GET.get('keywords', '')
+        # if search_keywords:
+        #     # icontains:类似mysql的like语句，i表示不区分大小写
+        #     all_courses = all_courses.filter(Q(name__icontains=search_keywords) | Q(desc__icontains=search_keywords) |
+        #                                      Q(detail__icontains=search_keywords))
+
+        # 课程排序
+        # 获取排序类型
+        # sort = request.GET.get('sort', '')
+        # if sort:
+        #     if sort == "students":
+        #         all_courses = all_courses.order_by('-students')
+        #     elif sort == "hot":
+        #         all_courses = all_courses.order_by('-click_nums')
+
+        # 对课程进行分页
+        # try:
+        #     page = request.GET.get('page', 1)
+        # except PageNotAnInteger:
+        #     page = 1
+
+        # Provide Paginator with the request object for complete querystring generation
+        # 每页显示六个课程
+        # p = Paginator(all_articles, 6, request=request)
+        #
+        # courses = p.page(page)
+
+        return render(request, 'article_list.html', {
+            'all_articles': all_articles,
+            # 'sort': sort,
+            'hot_articles': hot_articles
+        })
+
